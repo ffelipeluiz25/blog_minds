874f7621d1404dbeb6939c21d602679153aba26d
Luiz Felipe Ribeiro dos Santos <luiz.felipe@mindsdigital.net>
2022-05-27 18:32:06 -0300

    components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Header } from './components/header';
-import { Main } from './components/main';
-import { Footer } from './components/footer';
+import { Header } from '../components/header';
+import { Main } from '../components/main';
+import { Footer } from '../components/footer';
 
 export const App = () => {
  return (
